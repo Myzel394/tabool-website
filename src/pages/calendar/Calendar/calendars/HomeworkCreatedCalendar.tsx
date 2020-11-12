deb35c8005aa06b29b5c95f64f35c71d8dde5c64
myzel394 <myzel394.xyllian@gmail.com>
2020-11-12 21:18:43 +0100

    Development

--- src/pages/calendar/Calendar/calendars/HomeworkCreatedCalendar.tsx
+++ src/pages/calendar/Calendar/calendars/HomeworkCreatedCalendar.tsx
@@ -1,12 +1,10 @@
 import {IDefaultCalendarManager} from "../DefaultCalendar";
-import {useFetchHomeworkListAPI} from "hooks/apis/fetch/homework";
 
 export interface IHomeworkCreatedCalendar extends IDefaultCalendarManager {}
 
 
 const HomeworkCreatedCalendar = ({activeView, onCalendarTypeChange, onViewChange}: IHomeworkCreatedCalendar) => {
-    const {data} = {};
-}
+};
 
 
 export default HomeworkCreatedCalendar;
