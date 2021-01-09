e1905f359ee1ba90d6cd38896d2b9d2d2093027c
myzel394 <miguel.kra@gmail.com>
2021-01-09 19:58:13 +0100

    FocusedPage improved

--- src/components/pages/FocusedPage/index.tsx
+++ src/components/pages/FocusedPage/index.tsx
@@ -1,13 +1,13 @@
-import React, {ReactNode, useContext} from "react";
-import {Box, Container, useTheme} from "@material-ui/core";
+import React, {ReactNode} from "react";
+import {Box} from "@material-ui/core";
 import {BackButton, Tooltip} from "components";
 import {useTranslation} from "react-i18next";
 
 import Logo from "../../Logo";
 import BasePage from "../BasePage";
-import {UtilsContext} from "../../../contexts";
 
 import Title from "./Title";
+import Wrapper from "./Wrapper";
 
 export interface IFocusedPage {
     children: ReactNode;
@@ -20,46 +20,32 @@ export interface IFocusedPage {
 
 export default function FocusedPage({children, title, important, disableBackButton, showLogo}: IFocusedPage) {
     const {t} = useTranslation();
-    const {bottomSheetHeight} = useContext(UtilsContext);
-    const theme = useTheme();
-    const containerStyles = {
-        backgroundColor: theme.palette.background.paper,
-        minHeight: `calc(100vh - ${bottomSheetHeight}px)`,
-    };
 
     return (
         <BasePage>
-            <Box
-                style={containerStyles}
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center"
-            >
-                <Container maxWidth="sm">
-                    <Box marginY={1} marginX={2}>
-                        {!disableBackButton &&
-                            <Box display="flex" justifyContent="flex-end">
-                                <Tooltip title={t("Zurück gehen").toString()}>
-                                    <>
-                                        {/* eslint-disable-next-line @shopify/jsx-prefer-fragment-wrappers */}
-                                        <span>
-                                            <BackButton confirm={important} />
-                                        </span>
-                                    </>
-                                </Tooltip>
-                            </Box>
-                        }
-                        {title &&
-                            <>
-                                {showLogo && <Logo />}
-                                <Title title={title} />
-                            </>
-                        }
-                        {children}
-                    </Box>
-                </Container>
-            </Box>
+            <Wrapper>
+                <Box marginY={1} marginX={2}>
+                    {!disableBackButton &&
+                        <Box display="flex" justifyContent="flex-end">
+                            <Tooltip title={t("Zurück gehen").toString()}>
+                                <>
+                                    {/* eslint-disable-next-line @shopify/jsx-prefer-fragment-wrappers */}
+                                    <span>
+                                        <BackButton confirm={important} />
+                                    </span>
+                                </>
+                            </Tooltip>
+                        </Box>
+                    }
+                    {title &&
+                        <>
+                            {showLogo && <Logo />}
+                            <Title title={title} />
+                        </>
+                    }
+                    {children}
+                </Box>
+            </Wrapper>
         </BasePage>
     );
 }
--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,3 +1,4 @@
 export {default as FocusedPage} from "./FocusedPage";
 export {default as DetailPage} from "./DetailPage";
 export {default as SearchPage} from "./SearchPage";
+export {default as BasePage} from "./BasePage";
