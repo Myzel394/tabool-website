1c4e24f2aa293e637de508d31d8125cea27aa1db
myzel394 <miguel.kra@gmail.com>
2021-02-09 19:07:00 +0100

    utils types added

--- src/api/convertToDate.ts
+++ src/api/convertToDate.ts
@@ -1,15 +1,15 @@
 import _ from "lodash";
 import dayjs from "dayjs";
 
-const convertToDate = (response, paths: string[]): void => {
+type ValueOf<T> = T[keyof T];
+
+const convertToDate = <T extends Record<string, any>>(response: T, paths: ValueOf<T>[]): void => {
     for (const path of paths) {
         const value = _.get(response, path);
 
-        if (typeof value === "string") {
-            const valueAsDate = dayjs(value, ["YYYY-MM-DDTHH:mm:ss", "YYYY-MM-DD", "HH:mm:ss"]);
+        const valueAsDate = dayjs(value, ["YYYY-MM-DDTHH:mm:ss", "YYYY-MM-DD", "HH:mm:ss"]);
 
-            _.set(response, path, valueAsDate);
-        }
+        _.set(response, path, valueAsDate);
     }
 };
 
