a7215460af9e64a6f65bc89c8a4e37e37832c982
myzel394 <miguel.kra@gmail.com>
2021-02-21 19:01:17 +0100

    classbook videoConferenceLink fix

--- src/types/classbook.d.ts
+++ src/types/classbook.d.ts
@@ -3,11 +3,13 @@ import {StudentLessonDateMixin, TeacherLessonDateMixin} from "./lesson";
 export interface StudentClassbook extends StudentLessonDateMixin {
     presenceContent: string;
     onlineContent: string;
+    videoConferenceLink: string;
     id: string;
 }
 
 export interface TeacherClassbook extends TeacherLessonDateMixin {
     presenceContent: string;
     onlineContent: string;
+    videoConferenceLink: string;
     id: string;
 }
