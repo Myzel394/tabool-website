78a4677a18976dd3674af893b439e30c15fd8799
myzel394 <miguel.kra@gmail.com>
2021-03-18 15:53:20 +0100

    queryOptions improvements

--- src/hooks/useQueryOptions.ts
+++ src/hooks/useQueryOptions.ts
@@ -5,17 +5,30 @@ import useSaveData from "./useSaveData";
 
 type ConfigType = UseQueryOptions<any, any, any>;
 
+const NO_RETRY_CODES = [
+    404,
+    403,
+    401,
+];
+
 const useQueryOptions = (): ConfigType => {
     const saveData = useSaveData();
+    // noinspection UnnecessaryLocalVariableJS
     const options = useMemo<ConfigType>(() => {
-        return saveData
-            ? {
+        let defaultData: ConfigType = {
+            retry: (failureCount, error) => failureCount <= 3 && !NO_RETRY_CODES.includes(error.response?.status),
+        };
+
+        if (saveData) {
+            defaultData = {
+                ...defaultData,
                 refetchOnWindowFocus: false,
                 refetchOnMount: true,
-                retry: 1,
                 refetchOnReconnect: false,
-            }
-            : {};
+            };
+        }
+
+        return defaultData;
     }, [saveData]);
 
     return options;
