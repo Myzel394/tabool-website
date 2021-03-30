d69992b48ef40c2632d774c390126f80b4f5eb07
myzel394 <miguel.kra@gmail.com>
2021-03-30 20:04:52 +0200

    default language getter improved

--- src/utils/getLanguageFromI18Next.ts
+++ src/utils/getLanguageFromI18Next.ts
@@ -1,5 +1,12 @@
 import i18next from "i18next";
 
-const getLanguageFromI18Next = (): string => i18next.language || window.localStorage.i18nextLng;
+const getLanguageFromI18Next = (): string => {
+    const savedValue = i18next.language || window.localStorage.i18nextLng;
+
+    if (!savedValue || savedValue === "_automatic") {
+        return "de";
+    }
+    return savedValue;
+};
 
 export default getLanguageFromI18Next;
