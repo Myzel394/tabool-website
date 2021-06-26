a4aa93ace1599e8b009d22a1006c2ec6b700b5ed
myzel394 <miguel.kra@gmail.com>
2021-06-26 14:17:47 +0200

    margin improved

--- src/pages/detail/LessonDetailPage/Teacher/RelatedObjects/Submissions/index.tsx
+++ src/pages/detail/LessonDetailPage/Teacher/RelatedObjects/Submissions/index.tsx
@@ -23,29 +23,29 @@ const Submissions = () => {
                 {t("Einsendungen")}
             </Typography>
             <Box my={2}>
-                <Typography variant="body1" color="textSecondary">
-                    {t("{{amount}} / {{users}} Einsendungen eingereicht", {
-                        amount: usersSubmissionAmount,
-                        users: lesson.course.participantsCount,
-                    })}
-                </Typography>
-                <Box mt={1}>
-                    <LinearProgress variant="determinate" value={Math.min(100, usersSubmissionAmount / lesson.course.participantsCount * 100)} />
-                </Box>
+                {submissions.length ? (
+                    <>
+                        <Typography variant="body1" color="textSecondary">
+                            {t("{{amount}} / {{users}} Einsendungen eingereicht", {
+                                amount: usersSubmissionAmount,
+                                users: lesson.course.participantsCount,
+                            })}
+                        </Typography>
+                        <Box mt={1}>
+                            <LinearProgress variant="determinate" value={Math.min(100, usersSubmissionAmount / lesson.course.participantsCount * 100)} />
+                        </Box>
+                        <List>
+                            {submissions.map(submission =>
+                                <TeacherSubmissionListElement key={submission.id} submission={submission} />)}
+                        </List>
+                        <ExtraOptions />
+                    </>
+                ) : (
+                    <Alert severity="info">
+                        {t("Es liegen keine Einsendungen vor.")}
+                    </Alert>
+                )}
             </Box>
-            {submissions.length ? (
-                <>
-                    <List>
-                        {submissions.map(submission =>
-                            <TeacherSubmissionListElement key={submission.id} submission={submission} />)}
-                    </List>
-                    <ExtraOptions />
-                </>
-            ) : (
-                <Alert severity="info">
-                    {t("Es liegen keine Einsendungen vor.")}
-                </Alert>
-            )}
         </>
     );
 };
